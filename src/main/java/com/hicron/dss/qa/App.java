c1030b177ba2a86b7eeadcdf36d9d11ce4ab7713
Maciej Lorenc <maciej.lorenc@hicron.com>
2022-01-24 10:11:31 +0100

    mvn archetype created

---
+++ src/main/java/com/hicron/dss/qa/App.java
@@ -0,0 +1,13 @@
+package com.hicron.dss.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
