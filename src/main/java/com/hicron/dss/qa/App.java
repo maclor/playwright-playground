e1dc45b0e12250771dc1466e2098995c744d7ab4
Maciej Lorenc <maciej.lorenc@hicron.com>
2022-01-24 11:13:29 +0100

    First Playwright run

--- src/main/java/com/hicron/dss/qa/App.java
+++ src/main/java/com/hicron/dss/qa/App.java
@@ -1,13 +1,21 @@
 package com.hicron.dss.qa;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            System.out.println(page.title());
+        }
     }
 }
