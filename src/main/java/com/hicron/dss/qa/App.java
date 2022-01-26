9bd13f5f878ff586fee285ff0c4013992e261ada
Maciej Lorenc <maciej.lorenc@hicron.com>
2022-01-26 12:32:32 +0100

    Sample junit test

--- src/main/java/com/hicron/dss/qa/App.java
+++ src/main/java/com/hicron/dss/qa/App.java
@@ -1,6 +1,7 @@
 package com.hicron.dss.qa;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
@@ -12,9 +13,12 @@ public class App
 {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch();
+            final BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions().setHeadless(false)
+                    .setSlowMo(50);
+            Browser browser = playwright.chromium().launch(launchOptions);
             Page page = browser.newPage();
             page.navigate("http://playwright.dev");
+            page.pause(); // opens Playwright inspector
             System.out.println(page.title());
         }
     }
